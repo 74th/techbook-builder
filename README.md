9a42dbfa369ab4a2a2cc2e15d4bfc67d9e79dca9
74th <site@74th.tech>
2020-07-12 11:32:09 +0900

    up

---
+++ README.md
@@ -0,0 +1,66 @@
+# @74th の 技術書典用ビルドツール
+
+@74th が技術書典用の本を作る時に使っているツールの共有です。
+ちゃんと整えていませんが、どんな感じに使っているかどうか解ると思います。
+
+以前は Re:View を使っていましたが、ここ 3 冊はこれを使っています。
+
+## なぜこれを使っているか
+
+- Markdown ベースで入稿データを作りたい。
+- Markdown だと表現力が足りない部分を、HTML で補完したい。
+- Re:View だと表現力が足りない部分を、LaTeX で補完するのは辛かったから。
+
+## 主に使っているツール
+
+- [markdown-pdf-cli](https://github.com/74th/markdown-pdf-cli) : [VS Code の Markdown を PDF に変換するツール](https://marketplace.visualstudio.com/items?itemName=yzane.markdown-pdf) が、絵文字や PlantUML に対応していて、素敵なので Markdown から HTML にする部分に使っています。
+- [Vivlio Style](https://vivliostyle.org/) : CSS 組版。
+- Chrome Browser : Vivlio Style HTML -> PDF
+- macOS Preview : PDF への文字の埋め込み
+
+## 必要なもの
+
+- Python 3.6 以降
+- Node.js
+- Chrome Browser
+- macOS
+
+## 環境の準備
+
+```
+# タスクランナー Invoke のインストール
+pip install invoke
+
+# NPM のツールのインストール
+npm install -g yarn
+yarn
+```
+
+## ビルド方法
+
+- `inv build`
+  - markdown-pdf-cli を使って、 HTML の生成
+- `inv open-vivliostyle`
+  - Vivlio Style のダウンロードと、http-server の実行
+- Chrome で右ページを開く http://127.0.0.1:8000/vivliostyle-viewer/vivliostyle-viewer.html#b=http://127.0.0.1:8000/contents.html&renderAllPages=true&userStyle=data:,/*%3Cviewer%3E*/%0A@page%20%7B%20size:%20A5;%20%7D%0A/*%3C/viewer%3E*/
+  - Vivlio Style を開き、生成された HTML へのレンダリング指示、A5 サイズなどの指定が URL に入っている
+- Chrome で Ctrl+P を押して、PDF として印刷
+- PDF を macOS の Preview で開き、export PDF で PDF を PDF として出力
+  - フォントの埋め込み
+- macOS の Preview で、annotation でテキストの追加ができるので、ページ番号の入っていないページに、ページ境界にページ番号を書き込む（[ノンブルの追加](http://www.nikko-pc.com/offset/faq/off-faq.html#3-1)）
+
+## 細かい設定など
+
+- markdown-pdf-cli の設定 [settings.yaml](./settings.yaml)
+  - https://github.com/yzane/vscode-markdown-pdf#options の設定を CLI では YAML に落としている
+- タスク [tasks.py](./tasks.py)
+  - chapters に章番号と、マークダウンファイルの対応表が書かれている。0 となっているものは、章番号をふらないもの。
+  - `# xxxx`が`<h1>xxxx</h1>`と h1 タグになるので、章の見出しと判断し、少番号の付与と、HTML のタイトル（Vivlio Style ではヘッダータイトルに使われる）に追加。
+  - `## xxxx`が`<h2>xxxx</h2>`と h2 タグになるので、節の見出しと判断し、節番号`3.2`を付与。
+  - h1、h2 タグで収集したデータを使って、[目次ページ ./contents.html](./contents.html)を作成。
+  - というような体裁のがちゃがちゃを、HTML を XML ファイルとみなして、Python でやっています。
+
+## 諦めていること、今やっていないこと
+
+- 節見出しのヘッダーへの追加。Vivlio Style では現状ファイルを分けなければ見出しを変えられないため。
+- ePub の生成。
---
+++ tasks.py
@@ -0,0 +1,118 @@
+#%%
+from typing import List, Tuple
+import glob
+import xml.dom.minidom as dom
+import os
+import invoke
+from invoke import task
+
+chapters = [
+    ("article/0.pre", 0),
+    ("article/1.file", 1),
+    ("article/2.search", 2),
+    ("article/3.git", 3),
+    ("article/4.auto", 4),
+    ("article/5.environments", 5),
+    ("article/6.view", 6),
+    ("article/7.keyboard-shortcut", 7),
+    ("article/8.other", 8),
+    ("article/9.favorite", 9),
+    ("article/99.end", 0),
+    ("article/99.author", 0),
+]
+
+#%%
+@task(default=True)
+def build(c):
+
+    with open(f"contents.html") as f:
+        d: dom.Document = dom.parse(f)
+    ul: dom.Node = d.getElementsByTagName("ul")[0]
+    for n in list(ul.childNodes):
+        ul.removeChild(n)
+
+    for chap in chapters:
+        build_html(c, chap)
+        title, sub_tiles = convert_html(invoke.Context, chap)
+        li = d.createElement("li")
+        ul.appendChild(li)
+        a = d.createElement("a")
+        li.appendChild(a)
+        a.setAttribute("href", f"{chap[0]}.xml.html")
+        a.setAttribute("class", "tocxref")
+        t = d.createTextNode(title)
+        a.appendChild(t)
+        if sub_tiles:
+            ul2 = d.createElement("ul")
+            li.appendChild(ul2)
+            for sub_title in sub_tiles:
+                li2 = d.createElement("li")
+                ul2.appendChild(li2)
+                t2 = d.createTextNode(sub_title)
+                li2.appendChild(t2)
+
+    with open(f"contents.html", "w") as f:
+        d.writexml(f)
+    c.run("./node_modules/.bin/prettier --write contents.html")
+
+
+#%%
+def build_html(c, chap):
+    markdown_pdf = "node_modules/.bin/markdown-pdf"
+    c.run(f"{markdown_pdf} html settings.yaml {chap[0]}.md")
+
+
+def convert_html(c, chap) -> Tuple[str, List[str]]:
+    with open(f"{chap[0]}.html") as f:
+        html_text = f.read()
+    cwd = os.getcwd()
+    html_text = html_text.replace(f"file://{cwd}", "..")
+    d = dom.parseString(html_text)
+
+    title = ""
+    sub_tiles = []
+    # if chap[1] > 0:
+    if True:
+        # タイトルを挿げ替え
+        for tag in d.getElementsByTagName("h1"):
+            title_text = tag.firstChild.data
+            if chap[1] > 0:
+                title = f"{chap[1]}. {title_text}"
+            else:
+                title = f"{title_text}"
+            tag.firstChild.data = title
+        d.getElementsByTagName("title")[0].firstChild.data = title
+
+        for i, tag in enumerate(d.getElementsByTagName("h2")):
+            if chap[1] > 0:
+                sub_title = f"{chap[1]}.{i+1} {tag.firstChild.data}"
+            else:
+                sub_title = f"{tag.firstChild.data}"
+            tag.firstChild.data = sub_title
+            sub_tiles.append(sub_title)
+    else:
+        d.getElementsByTagName("title")[0].firstChild.data = ""
+
+    with open(f"{chap[0]}.xml.html", "w") as f:
+        d.writexml(f)
+    return title, sub_tiles
+
+
+@task
+def open_vivliostyle(c):
+    ver = "2019.8.101"
+    url = "https://github.com/vivliostyle/vivliostyle/releases/download/2019.8.101/vivliostyle-js-2019.8.101.zip"
+    if not os.path.exists("vivliostyle-viewer"):
+        c.run(f"curl -OL {url}")
+        c.run(f"unzip vivliostyle-js-{ver}.zip")
+        c.run(f"mv vivliostyle-js-{ver}/viewer vivliostyle-viewer")
+        c.run(f"rm -rf vivliostyle-js-{ver} vivliostyle-js-{ver}.zip")
+    page = "http://127.0.0.1:8000/vivliostyle-viewer/vivliostyle-viewer.html#b=http://127.0.0.1:8000/contents.html&renderAllPages=true&userStyle=data:,/*%3Cviewer%3E*/%0A@page%20%7B%20size:%20A5;%20%7D%0A/*%3C/viewer%3E*/"
+    print(page)
+    # c.run(
+    #     'open /Applications/Google\\ Chrome.app "http://127.0.0.1:8000/vivliostyle-viewer/vivliostyle-viewer.html#b=http://127.0.0.1:8000/contents.html&renderAllPages=true&userStyle=data:,/*%3Cviewer%3E*/%0A@page%20%7B%20size:%20A5;%20%7D%0A/*%3C/viewer%3E*/"',
+    #     warn=True,
+    #     hide=True,
+    # )
+
+    c.run("npx http-server -p 8000 -c-1")
